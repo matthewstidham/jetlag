599d366d37bc08ae3b414da45b5e176a64ba4a9a
matthew <stidmatt@gmail.com>
2023-03-20 21:33:52 -0400

    Initial commit
    Signed-off-by: matthew <stidmatt@gmail.com>

---
+++ header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jet Lag: The Game</title>
+    </head>
+    <body>
+        <h1>Jet Lag</h1>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'header.php' ?>
+        <h2>Seasons</h2>
+        <ol>
+            <li>Connect 4 Across America</li>
+            <li>Circumnavigation</li>
+            <li>Europe</li>
+            <li>We Raced to Visit The Most US States<li>
+            <li>New Zealand</li>
+        </ol>
+<?php include 'footer.php' ?>
